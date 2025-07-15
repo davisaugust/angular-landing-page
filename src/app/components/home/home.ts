425bd37e2b15fc3ff7c59fc946a4a7ac8ebfdd5f
Davi Augusto <davisaugust004@Gmail.com>
2025-07-15 19:15:58 -0300

    feat: footer component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,10 +4,11 @@ import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimary } from '../btn-primary/btn-primary';
 import { NewsForm } from '../news-form/news-form';
 import { IlusExample } from '../ilus-example/ilus-example';
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Header,BtnPrimary,IlusExample , NewsForm ,NgOptimizedImage],
+  imports: [Header, BtnPrimary, IlusExample, NewsForm,Footer, NgOptimizedImage, Footer],
   providers: [],
   templateUrl: './home.html',
   styleUrl: './home.scss'
